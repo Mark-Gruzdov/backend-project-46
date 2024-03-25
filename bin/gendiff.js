a1d2e52a29a9713d2f30aac3964ee3457b2b70c3
Mark Gruzdov <grumark198@gmail.com>
2024-03-25 20:32:00 +0300

    second stage

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,10 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format');
+  //.argument('<filepath1>', 'first filepath')
+  //.argument('<filepath2>', 'second filepath');
+
 
   program.parse();
