6b1ab866b926963d6039646574d7bb1297fd202c
Mark Gruzdov <grumark198@gmail.com>
2024-04-02 22:35:47 +0300

    fix code smells

--- src/compare.js
+++ src/compare.js
@@ -7,26 +7,45 @@ const getSortedKeys = (obj1, obj2) => {
   return _.sortBy(unionKeys);
 };
 
+const getCase = (obj1, obj2, key) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
+  let result = 'case_5';
+  if (_.has(obj1, key) && !_.has(obj2, key)) {
+    result = 'case_1';
+  } if (!_.has(obj1, key) && _.has(obj2, key)) {
+    result = 'case_2';
+  } if (_.isObject(value1) && _.isObject(value2)) {
+    result = 'case_3';
+  } if (_.isEqual(value1, value2)) {
+    result = 'case_4';
+  }
+  return result;
+};
+
 const compare = (data1, data2) => {
   const keys = getSortedKeys(data1, data2);
-  const getCompareData = (obj1, obj2, key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    let result = {
-      key, value1, value2, state: 'differ',
-    };
-    if (_.has(obj1, key) && !_.has(obj2, key)) {
-      result = { key, value1, state: 'only in 1' };
-    } if (!_.has(obj1, key) && _.has(obj2, key)) {
-      result = { key, value2, state: 'only in 2' };
-    } if (_.isObject(value1) && _.isObject(value2)) {
-      result = { key, children: compare(value1, value2), state: 'nested' };
-    } if (_.isEqual(value1, value2)) {
-      result = { key, value1, state: 'matched' };
+  const result = keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    const condition = getCase(data1, data2, key);
+    switch (condition) {
+      case 'case_1':
+        return { key, value1, state: 'only in 1' };
+      case 'case_2':
+        return { key, value2, state: 'only in 2' };
+      case 'case_3':
+        return { key, children: compare(value1, value2), state: 'nested' };
+      case 'case_4':
+        return { key, value1, state: 'matched' };
+      case 'case_5':
+        return {
+          key, value1, value2, state: 'differ',
+        };
+      default:
+        throw new Error(`Unknown case ${condition}.`);
     }
-    return result;
-  };
-  const result = keys.map((key) => getCompareData(data1, data2, key));
+  });
   return result;
 };
 
--- src/output.js
+++ src/output.js
@@ -8,38 +8,34 @@ const getRightIndent = (depth, replacer = ' ', spacesCount = 4) => (
 const getString = (data, depth = 1) => {
   if (!_.isPlainObject(data)) return `${data}`;
   const currentValue = Object.entries(data);
-  const leftIndent = getLeftIdent(depth);
-  const rightIndent = getRightIndent(depth);
-  const line = currentValue.map(([key, value]) => `${leftIndent} ${key}: ${getString(value, depth + 1)}`);
-  return ['{', ...line, `${rightIndent}}`].join('\n');
+  const line = currentValue.map(([key, value]) => `${getLeftIdent(depth)} ${key}: ${getString(value, depth + 1)}`);
+  return ['{', ...line, `${getRightIndent(depth)}}`].join('\n');
 };
 
 const getOutput = (tree) => {
   const iter = (currentValue, depth = 1) => {
-    const leftIndent = getLeftIdent(depth);
-    const rightIndent = getRightIndent(depth);
     const line = currentValue.flatMap((node) => {
       const {
         key, children, state, value1, value2,
       } = node;
       switch (state) {
         case 'nested':
-          return `${leftIndent}  ${key}: ${iter(children, depth + 1)}`;
+          return `${getLeftIdent(depth)}  ${key}: ${iter(children, depth + 1)}`;
         case 'only in 1':
-          return `${leftIndent}- ${key}: ${getString(value1, depth + 1)}`;
+          return `${getLeftIdent(depth)}- ${key}: ${getString(value1, depth + 1)}`;
         case 'only in 2':
-          return `${leftIndent}+ ${key}: ${getString(value2, depth + 1)}`;
+          return `${getLeftIdent(depth)}+ ${key}: ${getString(value2, depth + 1)}`;
         case 'matched':
-          return `${leftIndent}  ${key}: ${getString(value1, depth + 1)}`;
+          return `${getLeftIdent(depth)}  ${key}: ${getString(value1, depth + 1)}`;
         case 'differ':
           return [
-            `${leftIndent}- ${key}: ${getString(value1, depth + 1)}`,
-            `${leftIndent}+ ${key}: ${getString(value2, depth + 1)}`];
+            `${getLeftIdent(depth)}- ${key}: ${getString(value1, depth + 1)}`,
+            `${getLeftIdent(depth)}+ ${key}: ${getString(value2, depth + 1)}`];
         default:
           throw new Error(`Unknown type ${state}.`);
       }
     });
-    return ['{', ...line, `${rightIndent}}`].join('\n');
+    return ['{', ...line, `${getRightIndent(depth)}}`].join('\n');
   };
   return iter(tree);
 };
